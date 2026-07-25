b65cad5054b2d73ff73e622c6a88dc5df775859c
Devon <devony07@students.cs.ubc.ca>
2026-07-24 21:46:17 -0700

    Added an allowed host

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,18 +3,21 @@ import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, './src'),
+    plugins: [react()],
+    resolve: {
+        alias: {
+            '@': resolve(__dirname, './src'),
+        },
     },
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8000',
-        changeOrigin: true,
-      },
+    server: {
+        proxy: {
+            '/api': {
+                target: 'http://localhost:8000',
+                changeOrigin: true,
+            },
+        },
+        allowedHosts: [
+            "sb-67scce6cftqu.vercel.run",
+        ],
     },
-  },
 })
